12aa2caeb5fdb001bcc3f3685a93ef71a21097ae
Oleksandr Pylypenko <oleksandr.pylypenko@lotusflare.com>
2019-03-17 23:41:06 +0200

    Add metrics lol

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
+from prometheus_client import start_http_server, Counter
+
+FEE_FUNC_CALLS_TOTAL = Counter('fee_func_calls_total', 'Amount of fee function calls')
+UNKNOWN_COMMANDS_TOTAL = Counter('unknown_command_total', 'Unknows commands entered')
+
 
 updater = Updater(token=os.environ['TG_TOKEN'])
 dispatcher = updater.dispatcher
@@ -13,17 +18,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 
 def calculate_fee(bot, update, args):
+    FEE_FUNC_CALLS_TOTAL.inc()
     amount = float(' '.join(args))
-    result = amount + (amount + amount * 0.0035) * 0.0035
-    bot.send_message(chat_id=update.message.chat_id, text=result)
+    result = round((amount + (amount + amount * 0.0035) * 0.0035), 2)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="{0} UAH".format(result))
 
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text="I'm a bot, please talk to me!")
+                     text="Hello.")
 
 
 def unknown(bot, update):
+    UNKNOWN_COMMANDS_TOTAL.inc()
     bot.send_message(chat_id=update.message.chat_id,
                      text="Sorry, I didn't understand that command.")
 
@@ -35,4 +43,5 @@ dispatcher.add_handler(start_handler)
 dispatcher.add_handler(fee_handler)
 dispatcher.add_handler(unknown_handler)
 
+start_http_server(8000)
 updater.start_polling()
